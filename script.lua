ac655a7530b2c5fb7bdc8bda14adac18298d3748
Leader150s <kankikanki831@gmail.com>
2026-04-11 01:27:50 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,7 +2,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "DMS HUB ⚡ إصدار ملك القراصنة",
-   LoadingTitle = "DMS HUB | GEAR 5",
+   LoadingTitle = "DMS HUB | اختيار السلاح",
    LoadingSubtitle = "بواسطة Leader150s",
    ConfigurationSaving = { Enabled = false },
    KeySystem = false,
@@ -11,17 +11,34 @@ local Window = Rayfield:CreateWindow({
 
 local MainTab = Window:CreateTab("🏠 القائمة", 4483362458)
 
+MainTab:CreateSection("⚔️ إعدادات الفارم")
+
+-- متغيرات الاختيار
+_G.Weapon = "أسلوب قتال" -- السلاح الافتراضي
 _G.farming = false
+
 MainTab:CreateToggle({
-   Name = "⚔️ تشغيل الفارم المطور (الملف الفرعي)",
+   Name = "تشغيل التلفيل التلقائي",
    CurrentValue = false,
    Flag = "FarmToggle",
    Callback = function(Value)
       _G.farming = Value
       if Value then
-          -- هنا الربط مع ملفك الثاني
           loadstring(game:HttpGet("https://raw.githubusercontent.com/Leader150s/LUR-Hub/refs/heads/main/MainFarm.lua"))()
       end
+   end,
+})
+
+-- قائمة اختيار السلاح
+MainTab:CreateDropdown({
+   Name = "إختر السلاح المستخدم",
+   Options = {"Melee","Sword","Blox Fruit"},
+   CurrentOption = {"Melee"},
+   MultipleOptions = false,
+   Flag = "WeaponDropdown",
+   Callback = function(Option)
+      _G.Weapon = Option[1]
+      Rayfield:Notify({Title = "DMS HUB", Content = "تم اختيار: " .. _G.Weapon, Duration = 2})
    end,
 })
 
